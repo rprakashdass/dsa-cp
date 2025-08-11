97fbc56a6de0d9706a479b254c3be5be666c1098
rprakashdass <rprakashdass@gmail.com>
2025-08-11 11:57:58 +0530

    24. Swap Nodes in Pairs

---
+++ Leetcode/24. Swap Nodes in Pairs/.py
@@ -0,0 +1,27 @@
+"""
+Time Complexity: O(N)
+Space Complexity: O(1)
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev = dummy
+
+        while head and head.next:
+            first = head
+            second = head.next
+
+            prev.next = second
+            first.next = second.next
+            second.next = first
+
+            prev = first
+            head = first.next
+
+        return dummy.next
